fa620fdc6246ee25df9a7b3ee12b5004a7f875f0
Ashish-Goel007 <35141714+Ashish-Goel007@users.noreply.github.com>
2024-01-16 16:37:46 +0530

    Add files via upload

---
+++ multistage-docker/src/main/java/com/example/multistagedocker/MultistageDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.multistagedocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultistageDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultistageDockerApplication.class, args);
+	}
+
+}
---
+++ multistage-docker/src/test/java/com/example/multistagedocker/MultistageDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multistagedocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultistageDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
